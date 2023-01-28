6dcd93dcb02d47688654077f830a4f718721769e
DINH PHUONG NAM <81277541+nam0403@users.noreply.github.com>
2023-01-28 21:34:57 +0700

    update part 6

--- get_dataset.py
+++ get_dataset.py
@@ -3,29 +3,20 @@ import os
 import zipfile
 import tarfile
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='Process some dataset')
 parser.add_argument('--dataset_folder', type=str, default='dataset', help='Path to dataset folder')
 args = parser.parse_args()
-
-    
+URL = "https://www.robots.ox.ac.uk/~vgg/data/oxbuildings/oxbuild_images-v1.tgz"
+# 2. download the data behind the URL
+response = requests.get(URL)
+# 3. Open the response into a new file called instagram.ico
+open("file.tgz", "wb").write(response.content)
 folder_path = args.dataset_folder
-
-url = "https://www.robots.ox.ac.uk/~vgg/data/oxbuildings/oxbuild_images-v1.tgz"
-wget.download(url, "file.tgz")
-
 file = tarfile.open('file.tgz')
-  
 # extracting file
 file.extractall(folder_path)
-
-folder_path = 'dataset'
-image_ext = ('jpg', 'jpeg', 'png')
-img_paths = [os.path.join(folder_path,f) for f in os.listdir(folder_path) if f.endswith(image_ext)]
-
-def get_folder():
-    return img_paths
-
 file.close()
 
 
